35436f49fbe72b5fd17e667f952f462dbb3f4969
Riyuman <139713314+Riyuman@users.noreply.github.com>
2024-05-29 02:35:48 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -10,12 +10,12 @@ async def new_join_f(client, message):
     chat_type = message.chat.type
     if chat_type != "private":
         await message.reply_text(
-            f"""<b>🙋🏻‍♂️ Hello dear!\n\n Welcome to Leech Group</b>\n\n<b>Current CHAT ID: <code>{message.chat.id}</code>""",
+            f"""<b>🙋🏻‍♂️ Hello dear!\n\n Welcome to @Oogo_Nick Leech Bot</b>\n\n<b>Current CHAT ID: <code>{message.chat.id}</code>""",
             parse_mode="html",
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton('Channel', url='https://t.me/WARISZONE')
+                        InlineKeyboardButton('♠️OᖴᖴIᑕIᗩᒪ ᑕᕼᗩᑎᑎᗴᒪ', url='https://t.me/movie_provider_official')
                     ]
                 ]
                )
